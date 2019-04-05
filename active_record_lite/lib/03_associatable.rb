fcfa223419e19ceb015126ddc06a431fa596cec8
Bryce Morgan <bamorgan13@gmail.com>
2019-04-05 18:17:40 -0400

    Convert CSS from fluid grid to flex, implement associations in active_record_lite

--- active_record_lite/lib/03_associatable.rb
+++ active_record_lite/lib/03_associatable.rb
@@ -9,46 +9,62 @@ class AssocOptions
     :primary_key
   )
 
-  def model_class(name)
-    name.singularize.camelcase.constantize
+  def model_class
+    class_name.constantize
   end
 
   def table_name
-    self.to_s.tableize 
+    model_class.table_name 
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    @foreign_key = options[:foreign_key] || name.concat("Id").underscore
+    @foreign_key = options[:foreign_key] || (name.to_s + "Id").underscore.to_sym
     @primary_key = options[:primary_key] || :id
-    @class_name = options[:class_name] || name.singularize.camelcase
+    @class_name = options[:class_name] || name.to_s.singularize.camelcase
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    @foreign_key = options[:foreign_key] || self_class_name.concat("Id").underscore
+    @foreign_key = options[:foreign_key] || self_class_name.to_s.concat("Id").underscore.to_sym
     @primary_key = options[:primary_key] || :id
-    @class_name = options[:class_name] || model_class
+    @class_name = options[:class_name] || name.to_s.singularize.camelcase
   end
 end
 
 module Associatable
+  #attr_accessor :options
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
-  end
+    options = BelongsToOptions.new(name, options)
+    define_method(name) do 
+      fk = options.foreign_key.to_s
+      model = options.model_class
+      fk_val = self.send(fk)
+      model.where({options.primary_key => fk_val}).first
+    end
+    assoc_options[name] = options 
+end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, table_name.singularize.camelcase, options)
+    define_method(name) do 
+      pk = options.primary_key.to_s
+      model = options.model_class
+      primary_val = self.send(pk)
+      model.where({options.foreign_key => primary_val})
+    end
   end
 
   def assoc_options
     # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @options ||= {}
+    return @options 
   end
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable 
 end
